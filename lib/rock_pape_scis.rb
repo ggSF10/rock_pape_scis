1d3546440bf8dc83edc334a055038abe6f2303c6
George Garabedian <gmgarabedian@gmail.com>
2014-10-01 14:00:13 -0700

    RPS Ruby Gem files

--- lib/rock_pape_scis.rb
+++ lib/rock_pape_scis.rb
@@ -1,45 +1,28 @@
 require "rock_pape_scis/version"
 
 module RockPapeScis
-	
-	class Player
-		attr_reader :name, :move
-
-  	def initialize(name, move = nil)
-      @name = name
-      @move = move
-  	end
-
-  end
-
   class Game
 
-  	def play(player1, player2)
-  		puts("#{player1.name} please choose rock, paper or scissors?")
-  		player1[move] = gets.chomp
-
-  		puts("#{player2.name} please choose rock, paper or scissors?")
-  		player1[move] = gets.chomp
-
-  		if player1[move] == player2[move]
+  	def self.play(player1, player2)
+  		if player1[:move] == player2[:move]
   			return "It's a tie"
-  		elsif player1[move] == "rock"
-  			if player2[move] == "scissors"
-  				return "#{player1.name} wins with rock!"
+  		elsif player1[:move] == "rock"
+  			if player2[:move] == "scissors"
+  				return "#{player1[:name]} wins with rock!"
   			else
-  				return "#{player2.name} wins with paper!"
+  				return "#{player2[:name]} wins with paper!"
   			end
-  		elsif player1[move] == "paper"
-  			if player2[move] == "rock"
-  				return "#{player1.name} wins with paper!"
+  		elsif player1[:move] == "paper"
+  			if player2[:move] == "rock"
+  				return "#{player1[:name]} wins with paper!"
   			else
-  				return "#{player2.name} wins with scissors!"
+  				return "#{player2[:name]} wins with scissors!"
   			end
-			elsif player1[move] == "scissors"
-				if player2[move] == "rock"
-					return "#{player2.name} wins with rock"
+			elsif player1[:move] == "scissors"
+				if player2[:move] == "rock"
+					return "#{player2[:name]} wins with rock!"
 				else
-					return "#{player1.name} wins with scissors"
+					return "#{player1[:name]} wins with scissors!"
 				end
 			end
   	end
--- spec/rock_pape_scis_spec.rb
+++ spec/rock_pape_scis_spec.rb
@@ -1,31 +1,40 @@
 require_relative '../lib/rock_pape_scis.rb'
 require_relative 'spec_helper.rb'
 
-describe RockPapeScis do
-	let(:player1) {RockPapeScis::Player.new("Bob")}
-	let(:player2) {RockPapeScis::Player.new("Jon")}
-	let(:game) {RockPapeScis::Game.new}
 
-	describe RockPapeScis::Player do
-		describe "#initialize" do
-			it "creates a new instance of a player" do
-    		expect(player1.name).to eq("Bob")  
-    		expect(player1.move).to eq(nil)
-			end
+describe RockPapeScis::Game do
+	describe "#play" do
+		it "determines a tie" do
+			player1 = { :move => "rock", :name => "George" }
+			player2 = { :move => "rock", :name => "Mike" }
+			result = RockPapeScis::Game.play(player1, player2)
+			expect(result).to eq("It's a tie")
 		end
-	end
 
-	describe RockPapeScis::Game do
-		describe "#play" do
-			it "determines a tie" do
-				player1[move] = "rock"
-				player1[move] = "rock"
-				result = game.play(player1, player2)
-				expect(result).to eq("It's a tie")
-			end
-
-
+		it "determines rock a winner vs. scissors" do
+			player1 = { :move => "rock", :name => "George" }
+			player2 = { :move => "scissors", :name => "Mike" }
+			result = RockPapeScis::Game.play(player1, player2)
+			expect(result).to eq("George wins with rock!")
 		end
+
+
+		it "determines paper a winner vs. rock" do
+			player1 = { :move => "paper", :name => "George" }
+			player2 = { :move => "rock", :name => "Mike" }
+			result = RockPapeScis::Game.play(player1, player2)
+			expect(result).to eq("George wins with paper!")
+		end
+
+
+		it "determines scissors a winner vs. paper" do
+			player1 = { :move => "scissors", :name => "George" }
+			player2 = { :move => "paper", :name => "Mike" }
+			result = RockPapeScis::Game.play(player1, player2)
+			expect(result).to eq("George wins with scissors!")
+		end
+
+
 	end
 end
 
